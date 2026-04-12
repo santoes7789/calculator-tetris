6439e2419e2302b52c53878a96d083e57ded6f6c
aj <jamesa7789@gmail.com>
2026-04-12 13:37:31 +1200

    changing I shape

--- src/tetrominos.c
+++ src/tetrominos.c
@@ -8,5 +8,5 @@ const TetData tetrominos[7] = {
     [3] = {.size = 3, .name = SHAPE_Z, .data = {true, true, false, false, true, true, false, false, false, false, false, false}}, // Z
     [4] = {.size = 3, .name = SHAPE_T, .data = {true, true, true, false, true, false, false, false, false, false, false, false}}, // T
     [5] = {.size = 3, .name = SHAPE_O, .data = {true, true, false, true, true, false, false, false, false, false, false, false}}, // O
-    [6] = {.size = 4, .name = SHAPE_I, .data = {true, true, true, true, false, false, false, false, false, false, false, false}} // I
+    [6] = {.size = 4, .name = SHAPE_I, .data = {false, false, false, false, true, true, true, true, false, false, false, false}} // I
 };
